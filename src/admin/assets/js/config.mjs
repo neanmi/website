8b25f811668fe0177881fc8b0fc149e904447902
Stefan <stefan@stefanpuia.co.uk>
2022-06-12 12:57:31 +0100

    features services CMS closes #14 closes #13

--- src/admin/assets/js/config.mjs
+++ src/admin/assets/js/config.mjs
@@ -1,6 +1,12 @@
 import { ConfigErrorsPreview } from "./preview/config-errors.mjs";
+import { ConfigFeaturesPreview } from "./preview/config-features.mjs";
+import { FeaturePreview } from "./preview/feature.mjs";
+import { ServicePreview } from "./preview/service.mjs";
 
 CMS.registerPreviewStyle("/assets/css/main.min.css");
 CMS.registerPreviewStyle("/admin/assets/css/preview.css");
 
 CMS.registerPreviewTemplate("config-errors", ConfigErrorsPreview);
+CMS.registerPreviewTemplate("config-features", ConfigFeaturesPreview);
+CMS.registerPreviewTemplate("services", ServicePreview);
+CMS.registerPreviewTemplate("features", FeaturePreview);
